a2a3123d123643b482064033d78a8434f2c5eff2
Cédric Belin <cedric@belin.io>
2016-11-01 01:00:58 +0100

    Plug-in options are optional :)

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ export * from './reporter';
 
 /**
  * Create a new instance of the plug-in.
- * @param {object} options The plug-in options.
+ * @param {object} [options] The plug-in options.
  * @return {Checker} Tthe newly created instance.
  */
-export function david(options) {
+export function david(options = {}) {
   return new Checker(options);
 }
