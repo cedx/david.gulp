cbdb20f298ac7ef88de1da617f3765e0bc89fc13
Cédric Belin <cedric@belin.io>
2016-11-04 01:26:03 +0100

    Fixed a typo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export * from './reporter';
 /**
  * Create a new instance of the plug-in.
  * @param {object} [options] The plug-in options.
- * @return {Checker} Tthe newly created instance.
+ * @return {Checker} The newly created instance.
  */
 export function david(options = {}) {
   return new Checker(options);
