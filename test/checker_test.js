e95099bbddd7f38be49204ee6497ea2f262f0475
Cédric Belin <cedric@belin.io>
2017-08-02 03:16:55 +0200

    Moved the `factory()` function to the `Checker.factory` method

--- test/checker_test.js
+++ test/checker_test.js
@@ -2,16 +2,55 @@
 
 const {expect} = require('chai');
 const {Readable} = require('stream');
+const {URL} = require('url');
 const File = require('vinyl');
 
 const pkg = require('../package.json');
-const {Checker} = require('../lib');
+const {Checker, Reporter} = require('../lib');
 
 /**
  * @test {Checker}
  */
 describe('Checker', function() {
   this.timeout(15000);
+
+  /**
+   * @test {Checker.factory}
+   */
+  describe('.factory()', () => {
+    it('should return a `Checker` with a `Reporter`', () => {
+      let checker = Checker.factory();
+      expect(checker).to.be.instanceof(Checker);
+      expect(checker.reporter).to.be.instanceof(Reporter);
+    });
+
+    it('should properly initialize the instance properties', () => {
+      let checker = Checker.factory({
+        error404: true,
+        errorDepCount: 123,
+        errorDepType: true,
+        errorSCM: true,
+        ignore: ['@cedx/gulp-david'],
+        registry: 'https://github.com/cedx/gulp-david',
+        reporter: {foo: 'bar'},
+        unstable: true,
+        update: '='
+      });
+
+      expect(checker.error).to.deep.equal({
+        404: true,
+        depCount: 123,
+        depType: true,
+        scm: true
+      });
+
+      expect(checker.ignore).to.include('@cedx/gulp-david');
+      expect(checker.registry).to.be.instanceOf(URL).and.have.property('href').that.equal('https://github.com/cedx/gulp-david');
+      expect(checker.reporter).to.be.an('object').and.have.property('foo');
+      expect(checker.unstable).to.be.true;
+      expect(checker.update).to.equal('=');
+    });
+  });
 
   /**
    * @test {Checker#getDependencies}
