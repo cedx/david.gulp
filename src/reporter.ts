0663f0ba5c6ca3c8da22d51b52877daf37d48b2a
Cédric Belin <cedric@belin.io>
2018-08-05 22:51:15 +0200

    Starting the TypeScript port

---
+++ src/reporter.ts
@@ -0,0 +1,56 @@
+const chalk = require('chalk');
+const {EOL} = require('os');
+const pkg = require('../package.json');
+
+/**
+ * Prints the checker results to the standard output.
+ */
+class Reporter {
+
+  /**
+   * The class name.
+   * @type {string}
+   */
+  get [Symbol.toStringTag]() {
+    return 'Reporter';
+  }
+
+  /**
+   * Logs to the standard output the outdated dependencies provided by the specified file.
+   * @param {File} file The file providing the outdated dependencies.
+   * @throws {Error} The dependencies were not found in the file.
+   */
+  log(file) {
+    if (!('david' in file)) throw new Error(`[${pkg.name}] Dependencies not found.`);
+    console.log(this._report(file));
+  }
+
+  /**
+   * Builds the output of the outdated dependencies provided by the specified file.
+   * @param {File} file The file providing the outdated dependencies.
+   * @return {string} The output of the outdated dependencies.
+   */
+  _report(file) {
+    let lines = [];
+    lines.push(chalk.bold(file.path));
+
+    let types = Object.keys(file.david).filter(type => Object.keys(file.david[type]).length > 0);
+    if (!types.length) lines.push(chalk.green('  All dependencies up to date.'));
+    else for (let type of types) {
+      lines.push(type);
+
+      let deps = file.david[type];
+      for (let [name, dependency] of Object.entries(deps)) {
+        let requiredVersion = chalk.red(dependency.required || '*');
+        let stableVersion = chalk.green(dependency.stable || '*');
+        let latestVersion = chalk.yellow(dependency.latest || '*');
+        lines.push(`  ${chalk.magenta(name)} { required: ${requiredVersion}, stable: ${stableVersion}, latest: ${latestVersion} }`);
+      }
+    }
+
+    return lines.join(EOL);
+  }
+}
+
+// Module exports.
+exports.Reporter = Reporter;
--- test/reporter_test.ts
+++ test/reporter_test.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 const {expect} = require('chai');
 const File = require('vinyl');
 const {Reporter} = require('../lib/index.js');
