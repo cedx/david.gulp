2d3757e16ca3edfd1acbffbd9a893352bf1eacad
Cédric Belin <cedric@belin.io>
2018-08-26 01:41:55 +0200

    Advancing the TS port

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,16 +1,29 @@
-const chalk from 'chalk');
-const {EOL} from 'os');
+/* tslint:disable: no-console */
+import chalk from 'chalk';
+import {EOL} from 'os';
+import * as File from 'vinyl';
 // @ts-ignore: disable processing of the imported JSON file.
 import * as pkg from '../package.json';
 
 /**
+ * Defines the shape of a dependency reporter.
+ */
+export interface Reporter {
+
+  /**
+   * Logs the outdated dependencies provided by the specified file.
+   * @param file The file providing the outdated dependencies.
+   */
+  log(file: File): void;
+}
+
+/**
  * Prints the checker results to the standard output.
  */
-class Reporter {
+export class StdoutReporter implements Reporter {
 
   /**
    * The class name.
-   * @type {string}
    */
   get [Symbol.toStringTag](): string {
     return 'Reporter';
@@ -18,33 +31,33 @@ class Reporter {
 
   /**
    * Logs to the standard output the outdated dependencies provided by the specified file.
-   * @param {File} file The file providing the outdated dependencies.
+   * @param file The file providing the outdated dependencies.
    * @throws {Error} The dependencies were not found in the file.
    */
-  log(file) {
+  log(file: File): void {
     if (!('david' in file)) throw new Error(`[${pkg.name}] Dependencies not found.`);
     console.log(this._report(file));
   }
 
   /**
    * Builds the output of the outdated dependencies provided by the specified file.
-   * @param {File} file The file providing the outdated dependencies.
-   * @return {string} The output of the outdated dependencies.
+   * @param file The file providing the outdated dependencies.
+   * @return The output of the outdated dependencies.
    */
-  _report(file) {
-    let lines = [];
+  private _report(file: File): string {
+    const lines: string[] = [];
     lines.push(chalk.bold(file.path));
 
-    let types = Object.keys(file.david).filter(type => Object.keys(file.david[type]).length > 0);
+    const types = Object.keys(file.david).filter(type => Object.keys(file.david[type]).length > 0);
     if (!types.length) lines.push(chalk.green('  All dependencies up to date.'));
-    else for (let type of types) {
+    else for (const type of types) {
       lines.push(type);
 
-      let deps = file.david[type];
-      for (let [name, dependency] of Object.entries(deps)) {
-        let requiredVersion = chalk.red(dependency.required || '*');
-        let stableVersion = chalk.green(dependency.stable || '*');
-        let latestVersion = chalk.yellow(dependency.latest || '*');
+      const deps = file.david[type];
+      for (const [name, dependency] of Object.entries(deps)) {
+        const requiredVersion = chalk.red(dependency.required || '*');
+        const stableVersion = chalk.green(dependency.stable || '*');
+        const latestVersion = chalk.yellow(dependency.latest || '*');
         lines.push(`  ${chalk.magenta(name)} { required: ${requiredVersion}, stable: ${stableVersion}, latest: ${latestVersion} }`);
       }
     }
