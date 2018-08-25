9a4a69ff9dc6ff806a1e9f144a48b4801f11c49c
Cédric Belin <cedric@belin.io>
2018-08-26 01:41:55 +0200

    Advancing the TS port

--- src/checker.ts
+++ src/checker.ts
@@ -1,149 +1,168 @@
-const {getDependencies, getUpdatedDependencies} from 'david');
-const {Transform} from 'stream');
-const {promisify} from 'util');
-const {Reporter} from './reporter.js');
+import {getDependencies, getUpdatedDependencies} from 'david';
+import {Transform, TransformCallback} from 'stream';
+import {promisify} from 'util';
+import * as File from 'vinyl';
+
 // @ts-ignore: disable processing of the imported JSON file.
 import * as pkg from '../package.json';
+import {JsonMap} from './map';
+import {Reporter} from './reporter';
 
 /**
  * Checks whether the dependencies of a project are out of date.
  */
-class Checker extends Transform {
+export class Checker extends Transform {
+
+  /**
+   * The condition indicating that an error occurred.
+   */
+  error: ErrorCondition = {
+    404: false,
+    depCount: 0,
+    depType: false,
+    scm: false
+  };
+
+  /**
+   * The list of dependencies to ignore.
+   */
+  ignore: string[];
+
+  /**
+   * The [npm](https://www.npmjs.com) registry URL.
+   */
+  registry: URL;
+
+  /**
+   * The instance used to output the report.
+   */
+  reporter: Reporter;
+
+  /**
+   * Value indicating whether to use unstable dependencies.
+   */
+  unstable: boolean;
+
+  /**
+   * The operator to use in version comparators.
+   */
+  update: string;
+
+  /**
+   * Value indicating whether to output the versions of all dependencies instead of only the outdated ones.
+   */
+  verbose: boolean;
 
   /**
    * Initializes a new instance of the class.
-   * @param {Object} [options] An object specifying values used to initialize this instance.
+   * @param options An object specifying values used to initialize this instance.
    */
-  constructor({ignore = [], registry = null, reporter = null, unstable = false, update = '', verbose = false} = {}) {
+  constructor(options: Partial<CheckerOptions> = {}) {
     super({objectMode: true});
 
-    /**
-     * Values indicating whether to emit an error.
-     * @type {Object}
-     */
-    this.error = {
-      404: false,
-      depCount: 0,
-      depType: false,
-      scm: false
-    };
+    const {
+      ignore = [],
+      registry = 'https://registry.npmjs.org',
+      reporter = new Reporter,
+      unstable = false,
+      update = '',
+      verbose = false
+    } = options;
 
-    /**
-     * The list of dependencies to ignore.
-     * @type {string[]}
-     */
     this.ignore = ignore;
-
-    /**
-     * The [npm](https://www.npmjs.com) registry URL.
-     * Uses [registry.npmjs.org](https://registry.npmjs.org) if `null`.
-     * @type {URL}
-     */
-    this.registry = registry;
-
-    /**
-     * The instance used to output the report.
-     * @type {Object}
-     */
+    this.registry = typeof registry == 'string' ? new URL(registry) : registry;
     this.reporter = reporter;
-
-    /**
-     * Value indicating whether to use unstable dependencies.
-     * @type {boolean}
-     */
     this.unstable = unstable;
-
-    /**
-     * The operator to use in version comparators.
-     * If empty, the dependencies will not be updated.
-     * @type {string}
-     */
     this.update = update;
-
-    /**
-     * Value indicating whether to output the versions of all dependencies instead of only the outdated ones.
-     * @type {boolean}
-     */
     this.verbose = verbose;
   }
 
   /**
    * The class name.
-   * @type {string}
    */
   get [Symbol.toStringTag](): string {
     return 'Checker';
   }
 
   /**
-   * Create a new checker.
-   * @param {Object} [options] The checker options.
-   * @return {Checker} The newly created instance.
-   */
-  static factory(options = {}) {
-    let checker = new Checker({
-      ignore: Array.isArray(options.ignore) ? options.ignore : [],
-      registry: typeof options.registry == 'string' ? new URL(options.registry) : null,
-      reporter: typeof options.reporter == 'object' && options.reporter ? options.reporter : new Reporter,
-      unstable: typeof options.unstable == 'boolean' ? options.unstable : false,
-      update: typeof options.update == 'string' ? options.update : '',
-      verbose: typeof options.verbose == 'boolean' ? options.verbose : false
-    });
-
-    checker.error = {
-      404: typeof options.error404 == 'boolean' ? options.error404 : false,
-      depCount: Number.isInteger(options.errorDepCount) ? Math.max(0, options.errorDepCount) : 0,
-      depType: typeof options.errorDepType == 'boolean' ? options.errorDepType : false,
-      scm: typeof options.errorSCM == 'boolean' ? options.errorSCM : false
-    };
-
-    if (typeof options.reporter == 'boolean' && !options.reporter) checker.reporter = null;
-    if (typeof options.update == 'boolean' && options.update) checker.update = '^';
-    return checker;
-  }
-
-  /**
    * Gets details about project dependencies.
-   * @param {Object} manifest The manifest providing the dependencies.
-   * @return {Promise<Object>} An object providing details about the dependencies.
+   * @param manifest The manifest providing the dependencies.
+   * @return An object providing details about the dependencies.
    */
-  async getDependencies(manifest) {
+  async getDependencies(manifest: JsonMap): Promise<JsonMap> {
     return this._getDependencies(getDependencies, manifest);
   }
 
   /**
    * Gets details about project dependencies that are outdated.
-   * @param {Object} manifest The manifest providing the dependencies.
-   * @return {Promise<Object>} An object providing details about the dependencies that are outdated.
+   * @param manifest The manifest providing the dependencies.
+   * @return An object providing details about the dependencies that are outdated.
    */
-  async getUpdatedDependencies(manifest) {
+  async getUpdatedDependencies(manifest: JsonMap): Promise<JsonMap> {
     return this._getDependencies(getUpdatedDependencies, manifest);
   }
 
   /**
    * Parses the manifest contained in the specified file.
-   * @param {File} file The file to read.
-   * @param {string} [encoding] The file encoding.
-   * @return {Object} A manifest providing a list of dependencies.
+   * @param file The file to read.
+   * @param encoding The file encoding.
+   * @return A manifest providing a list of dependencies.
    * @throws {Error} The file is a stream, or the manifest is invalid.
    */
-  parseManifest(file, encoding = 'utf8') {
+  parseManifest(file: File, encoding: string = 'utf8'): JsonMap {
     if (file.isNull()) throw new Error(`Empty manifest: ${file.path}`);
     if (file.isStream()) throw new Error('Streams are not supported.');
 
-    let manifest = JSON.parse(file.contents.toString(encoding));
+    const manifest = JSON.parse((file.contents as Buffer).toString(encoding));
     if (typeof manifest != 'object' || !manifest) throw new Error('Invalid manifest format.');
     return manifest;
   }
 
   /**
+   * Transforms input and produces output.
+   * @param file The chunk to transform.
+   * @param encoding The encoding type if the chunk is a string.
+   * @param callback The function to invoke when the supplied chunk has been processed.
+   * @return The transformed chunk.
+   */
+  async _transform(file: File, encoding: string = 'utf8', callback?: TransformCallback): Promise<File> {
+    const getDeps = (mf: JsonMap) => this.verbose ? this.getDependencies(mf) : this.getUpdatedDependencies(mf);
+
+    try {
+      const manifest = this.parseManifest(file);
+      const deps = await getDeps(manifest);
+      file.david = deps;
+      if (this.reporter) this.reporter.log(file);
+
+      if (this.update.length) {
+        const version = this.unstable ? 'latest' : 'stable';
+        for (const type of Object.keys(deps))
+          for (const [name, dependency] of Object.entries(deps[type])) manifest[type][name] = this.update + dependency[version];
+
+        file.contents = Buffer.from(JSON.stringify(manifest, null, 2), encoding);
+      }
+
+      const count = Object.keys(deps).reduce((previousValue, type) => previousValue + Object.keys(deps[type]).length, 0);
+      if (this.error.depCount > 0 && count >= this.error.depCount) throw new Error(`Outdated dependencies: ${count}`);
+      if (callback) callback(undefined, file);
+    }
+
+    catch (err) {
+      if (callback) callback(new Error(`[${pkg.name}] ${err.message}`));
+      else throw err;
+    }
+
+    return file;
+  }
+
+  /**
    * Gets details about project dependencies.
    * @param {function} getter The function invoked to fetch the dependency details.
-   * @param {Object} manifest The manifest providing the list of dependencies.
-   * @return {Promise<Object>} An object providing details about the project dependencies.
+   * @param manifest The manifest providing the list of dependencies.
+   * @return An object providing details about the project dependencies.
    */
-  async _getDependencies(getter, manifest) {
-    let options = {
+  private async _getDependencies(getter, manifest: JsonMap): Promise<JsonMap> {
+    const options: JsonMap = {
       error: {E404: this.error['404'], EDEPTYPE: this.error.depType, ESCM: this.error.scm},
       ignore: this.ignore,
       loose: true,
@@ -155,7 +174,7 @@ class Checker extends Transform {
     };
 
     const getDeps = promisify(getter);
-    let [dependencies, devDependencies, optionalDependencies] = await Promise.all([
+    const [dependencies, devDependencies, optionalDependencies] = await Promise.all([
       getDeps(manifest, Object.assign({}, options, {dev: false, optional: false})),
       getDeps(manifest, Object.assign({}, options, {dev: true, optional: false})),
       getDeps(manifest, Object.assign({}, options, {dev: false, optional: true}))
@@ -163,41 +182,66 @@ class Checker extends Transform {
 
     return {dependencies, devDependencies, optionalDependencies};
   }
+}
+
+/**
+ * Defines the options of a `Checker` instance.
+ */
+export interface CheckerOptions {
 
   /**
-   * Transforms input and produces output.
-   * @param {File} file The chunk to transform.
-   * @param {string} [encoding] The encoding type if the chunk is a string.
-   * @param {function} [callback] The function to invoke when the supplied chunk has been processed.
-   * @return {Promise<File>} The transformed chunk.
+   * The list of dependencies to ignore.
    */
-  async _transform(file, encoding = 'utf8', callback = null) {
-    const getDeps = mf => this.verbose ? this.getDependencies(mf) : this.getUpdatedDependencies(mf);
+  ignore: string[];
 
-    try {
-      let manifest = this.parseManifest(file);
-      let deps = await getDeps(manifest);
-      file.david = deps;
-      if (this.reporter && typeof this.reporter.log == 'function') this.reporter.log(file, encoding);
+  /**
+   * The [npm](https://www.npmjs.com) registry URL.
+   */
+  registry: string | URL;
 
-      if (this.update.length) {
-        let version = this.unstable ? 'latest' : 'stable';
-        for (let type of Object.keys(deps))
-          for (let [name, dependency] of Object.entries(deps[type])) manifest[type][name] = this.update + dependency[version];
+  /**
+   * The instance used to output the report.
+   */
+  reporter: Reporter;
 
-        file.contents = Buffer.from(JSON.stringify(manifest, null, 2), encoding);
-      }
+  /**
+   * Value indicating whether to use unstable dependencies.
+   */
+  unstable: boolean;
 
-      let count = Object.keys(deps).reduce((previousValue, type) => previousValue + Object.keys(deps[type]).length, 0);
-      if (this.error.depCount > 0 && count >= this.error.depCount) throw new Error(`Outdated dependencies: ${count}`);
-      if (callback) callback(null, file);
-    }
+  /**
+   * The operator to use in version comparators.
+   */
+  update: string;
 
-    catch (err) {
-      if (callback) callback(new Error(`[${pkg.name}] ${err.message}`));
-      else throw err;
-    }
+  /**
+   * Value indicating whether to output the versions of all dependencies instead of only the outdated ones.
+   */
+  verbose: boolean;
+}
 
-    return file;
-  }
+/**
+ * Defines the condition indicating that an error occurred.
+ */
+export interface ErrorCondition {
+
+  /**
+   * If a dependency is not found, emit an error.
+   */
+  404: boolean;
+
+  /**
+   * If greater than `0`, emit an error when the count of outdated dependencies equals or exceeds the specified value.
+   */
+  depCount: number;
+
+  /**
+   * If a dependency version is invalid (not a string), emit an error.
+   */
+  depType: boolean;
+
+  /**
+   * If a dependency version is a source control URL, emit an error.
+   */
+  scm: boolean;
 }
