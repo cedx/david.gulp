47339c960a5c8410cd0f2722f61b441650636aa5
Cédric Belin <cedric@belin.io>
2018-08-26 19:14:42 +0200

    Advancing the TS port

--- test/reporter_test.ts
+++ test/reporter_test.ts
@@ -1,52 +1,50 @@
+/* tslint:disable: no-unused-expression */
 import {expect} from 'chai';
-const File from 'vinyl');
-const {Reporter} from '../lib';
+import {suite, test} from 'mocha-typescript';
+import * as File from 'vinyl';
+import {Reporter} from '../src';
 
 /**
- * Tests the `Reporter}
+ * Tests the features of the `Reporter` class.
  */
-describe('Reporter', () => {
+@suite class ReporterTest {
 
   /**
    * Tests the `Reporter#log}
    */
-  describe('#log()', () => {
-    it('should throw an error if the "david" property is not found on the file object', () => {
-      expect(() => (new Reporter).log(new File)).to.throw();
-    });
-  });
+  @test testLog(): void {
+    // It should throw an error if the "david" property is not found on the file object.
+    expect(() => (new Reporter).log(new File)).to.throw();
+  }
 
   /**
    * Tests the `Reporter#_report}
    */
-  describe('#_report()', () => {
-    let file = new File({contents: Buffer.from('{"name": "@cedx/gulp-david"}'), path: '/foo.js'});
+  @test testReport(): void {
+    const file = new File({contents: Buffer.from('{"name": "@cedx/gulp-david"}'), path: '/foo.js'});
     file.david = {};
 
-    it('should output the file path', () => {
-      let output = (new Reporter)._report(file);
-      expect(output).to.contain(file.path);
-    });
+    // It should output the file path.
+    let output = (new Reporter)._report(file);
+    expect(output).to.contain(file.path);
 
-    it('should output "All dependencies up to date." if there is no outdated dependencies', () => {
-      let output = (new Reporter)._report(file);
-      expect(output).to.contain(file.path);
-      expect(output).to.contain('All dependencies up to date.');
-    });
+    // It should output "All dependencies up to date." if there is no outdated dependencies.
+    output = (new Reporter)._report(file);
+    expect(output).to.contain(file.path);
+    expect(output).to.contain('All dependencies up to date.');
 
-    it('should output the package names and versions if there is some outdated dependencies', () => {
-      file.david = {
-        dependencies: {
-          foobar: {}
-        }
-      };
+    // It should output the package names and versions if there is some outdated dependencies.
+    file.david = {
+      dependencies: {
+        foobar: {}
+      }
+    };
 
-      let output = (new Reporter)._report(file);
-      expect(output).to.contain('dependencies')
-        .and.contain('foobar')
-        .and.contain('required:')
-        .and.contain('stable:')
-        .and.contain('latest:');
-    });
-  });
-});
+    output = (new Reporter)._report(file);
+    expect(output).to.contain('dependencies')
+      .and.contain('foobar')
+      .and.contain('required:')
+      .and.contain('stable:')
+      .and.contain('latest:');
+  }
+}
